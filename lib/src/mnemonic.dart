32a20dd4bd7d79e305cf65089cea23aeba02a6ba
joemarct <joemar.ct@gmail.com>
2020-11-07 05:22:00 +0800

    Set keylength to 32

--- lib/src/mnemonic.dart
+++ lib/src/mnemonic.dart
@@ -7,5 +7,5 @@ class Mnemonic {
   static String generate({int strength = 128}) => bip39.generateMnemonic(strength: strength);
 
   /// Create root seed from mnemonic
-  static Uint8List toSeed(String mnemonic) => bip39.mnemonicToSeed(mnemonic);
+  static Uint8List toSeed(String mnemonic) => bip39.mnemonicToSeed(mnemonic, 32);
 }
